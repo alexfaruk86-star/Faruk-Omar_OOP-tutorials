d8f89f51dc3fb245349148ac09f8a1b3fd75a46b
Faruk Omar <alexfaruk86@gmail.com>
2026-08-16 07:12:45 +0000

    Completed Week 6 tutorial

---
+++ week_6/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Lecturer l1 = new Lecturer(
+                "L100",
+                "Dr Ahmad",
+                "Java Programming",
+                "Faculty of Information Technology"
+        );
+
+
+        l1.displayInfo();
+
+        l1.displaySubject();
+
+    }
+}

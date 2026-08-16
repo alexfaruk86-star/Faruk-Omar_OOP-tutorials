fe4a6e997e0c2cfcfe57046dc9991cbc774fa65c
Faruk Omar <alexfaruk86@gmail.com>
2026-08-16 05:59:33 +0000

    Completed Week 2 tutorial

--- week_2/Main.java
+++ week_2/Main.java
@@ -1,1 +1,16 @@
+public class Main {
 
+    public static void main(String[] args) {
+
+        // Create Student object
+        Student s1 = new Student("John", 20, 3.5);
+
+        // Display information
+        s1.displayInfo();
+
+        // Call other methods
+        s1.study();
+
+        s1.takeExam();
+    }
+}
